bfabd22e3b72d0aacf68fae5b7937deeb87dc05d
1 <1>
2024-01-31 15:40:52 +0300

    Update

--- Demo.Authentication/CustomTicketStore.cs
+++ Demo.Authentication/CustomTicketStore.cs
@@ -2,29 +2,49 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Demo.Authentication
 {
     public class CustomTicketStore : ITicketStore
     {
+        private IMemoryCache _cache;
+
+        public CustomTicketStore(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+            
         public Task RemoveAsync(string key)
         {
-            throw new NotImplementedException();
+            _cache.Remove(key);
+            return Task.FromResult(0);
         }
 
         public Task RenewAsync(string key, AuthenticationTicket ticket)
         {
-            throw new NotImplementedException();
-        }
+            var options = new MemoryCacheEntryOptions();
+            var expiresUtc = ticket.Properties.ExpiresUtc;
+            if (expiresUtc.HasValue)
+            {
+                options.SetAbsoluteExpiration(expiresUtc.Value);
+            }
+            options.SetSlidingExpiration(TimeSpan.FromDays(100*356));//on server
+            _cache.Set(key, ticket, options);
+            return Task.FromResult(0);        }
 
         public Task<AuthenticationTicket> RetrieveAsync(string key)
         {
-            throw new NotImplementedException();
-        }
+            AuthenticationTicket ticket;
+            _cache.TryGetValue(key, out ticket);
+            return Task.FromResult(ticket);        }
 
-        public Task<string> StoreAsync(AuthenticationTicket ticket)
+        public async Task<string> StoreAsync(AuthenticationTicket ticket)
         {
-            throw new NotImplementedException();
+            var guid = Guid.NewGuid();
+            var key = DateTime.Now + guid.ToString();
+            await RenewAsync(key, ticket);
+            return key;
         }
     }
 }
